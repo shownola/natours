6439e12390ddab3842a88bd3329e341b5ce88620
shownola <swasieko@gmail.com>
2019-08-20 12:00:54 -0700

    Add middleware to serve static file

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 //MIDDLEWARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
