a15f71b610e3c3e7faecddc6ad9dc554c312de9f
shownola <swasieko@gmail.com>
2019-08-20 11:40:53 -0700

    Add validations with middleware functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,27 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if(req.params.id * 1 > tours.length){
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+    next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price){
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Must contain name and/or price'
+    });
+  }
+  next();
+}
+
 /////  GET  -  GET ALL TOURS ////////////////
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -20,13 +41,6 @@ exports.getTour = (req, res) => {
   console.log(req.params)
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
-  if (!tour){
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id'
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {tour}
@@ -45,32 +59,21 @@ exports.createTour = (req, res) => {
       status: 'success',
       data: {tour: newTour}
     });
-  });
-}
+    }
+  );
+};
 ////// PATCH - UDPATE TOUR /////
 
 exports.updateTour = (req, res) => {
-  if(req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {tour: '<Updated tour here...>'}
   });
-}
+};
 
 //////////  DELETE - DELETE TOUR //////////////////
 
 exports.deleteTour = (req, res) => {
-  if(req.params.id * 1 > tours.length){
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: {data: null}
