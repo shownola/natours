c94eb77c82b7cb03351bd3a25474748469220614
shownola <swasieko@gmail.com>
2019-08-20 13:08:21 -0700

    Set environment variables for development and production

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //MIDDLEWARES
-app.use(morgan('dev'));
+// console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+// console.log(process.env);
+
 // START SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
 });
