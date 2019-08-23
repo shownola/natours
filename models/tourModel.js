29eae07f8eba92eec8fea7b22acd15de4591ec63
shownola <swasieko@gmail.com>
2019-08-23 09:17:05 -0700

    Add Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -91,9 +91,16 @@ tourSchema.pre(/^find/, function(next){
 
 tourSchema.post(/^find/, function(docs, next){
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
   next();
 });
+
+// AGGREGATE MIDDLEWARE
+
+tourSchema.pre('aggregate', function(next){
+  this.pipeline().unshift({ $match: { secretTour: {$ne: true} } });  
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
