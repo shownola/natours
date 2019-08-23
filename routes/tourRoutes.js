1df24734ec29925fce0fe8915fab5337427dd427
shownola <swasieko@gmail.com>
2019-08-22 17:06:25 -0700

    Implement Aggregation Pipeline Matching and Grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
