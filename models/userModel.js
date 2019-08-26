0ea455a585e29f70b0821b09815ab7bb54c0fa06
shownola <swasieko@gmail.com>
2019-08-26 15:37:56 -0700

    Create user model and schema

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// Create schema with the following: name, email, photo, password, password confirmation
+const userSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, 'Enter your name']
+  },
+  email: {
+    type:: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide valid email']
+  },
+  photo: String,
+
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Must match password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
