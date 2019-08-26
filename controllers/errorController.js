6b901f67700d352392947526f3b6177baf67f20a
shownola <swasieko@gmail.com>
2019-08-26 11:54:45 -0700

    Handle errors in Development and Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCopde || 500;
-  err.status = err.status || 'error'
-
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    error: err,
+    message: err.message,
+    stack: err.stack
   });
 }
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming or other unknown error: don't give details to client
+  } else {
+    console.error('ERROR', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong! Try again'
+    });
+  }
+};
+
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if(process.env.NODE_ENV === 'development'){
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production'){
+    sendErrorProd(err, res);
+  }
+
+};
