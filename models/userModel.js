0f9f5176194cdf86a63c9bf193ab30db437b4ebe
shownola <swasieko@gmail.com>
2019-08-26 16:27:36 -0700

    Add bcrypt to manage passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // Create schema with the following: name, email, photo, password, password confirmation
 const userSchema = new mongoose.Schema ({
@@ -23,8 +24,25 @@ const userSchema = new mongoose.Schema ({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Must match password']
+    required: [true, 'Must match password'],
+    validate: {
+      // This only works on CREATE AND SAVE
+      validator: function(el){
+        return el === this.password;
+      },
+      message: 'Password must match'
+    }
   }
+});
+
+userSchema.pre('save', async function(next){
+  // Only run this function if password was modified
+  if(!this.isModified('password')) return next();
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
