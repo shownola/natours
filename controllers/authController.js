fb6fecf2fe557aef498bf376c32e8046f43e779b
shownola <swasieko@gmail.com>
2019-08-27 14:36:55 -0700

    Implement logging in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION
+  });
+};
 
 exports.signup = catchAsync(async(req, res, next) => {
   // const newUser = await User.create(req.body);
@@ -11,9 +18,8 @@ exports.signup = catchAsync(async(req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRATION
-  });
+  const token = signToken(newUser._id);
+
 
   res.status(201).json({
     status: 'success',
@@ -21,5 +27,27 @@ exports.signup = catchAsync(async(req, res, next) => {
     data: {
       user: newUser
     }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  // const email = req.body.email;
+  const { email, password } = req.body;   // using destructuring
+
+  // 1. check if email and password exist
+  if(!email || !password){
+    return next(new AppError('Please provide email and password', 400));
+  }
+  // 2. Check if user exists and password is correct
+  const user = await User.findOne({email}).select('+password');
+
+  if(!user || !(await user.correctPassword(password, user.password))){
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  // 3. If ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token
   });
 });
